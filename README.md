8bbb0bed91a19fe46c8906a2c647c17403a70bcf
skippor <125205616+skippor@users.noreply.github.com>
2025-11-11 07:59:29 +0000

    add: helloworld

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # driver
 linux kernel driver study and test 
+
+# development
+
+```shell
+# uname -r
+# sudo apt-get install linux-source-`uname -r`
+# sudo apt-get install linux-headers-`uname -r`
+sudo apt update
+sudo apt-get install build-essential
+sudo apt-get install linux-source-6.8.0
+sudo apt-get install linux-headers-6.8.0-1030
+```
---
+++ helloworld/helloworld.c
@@ -0,0 +1,23 @@
+#include <linux/init.h>             
+#include <linux/module.h>          
+#include <linux/kernel.h>   
+
+//指定license版本
+MODULE_LICENSE("GPL");              
+
+//设置初始化入口函数
+static int __init hello_world_init(void)
+{
+    printk(KERN_DEBUG "hello world!!!\n");
+    return 0;
+}
+
+//设置出口函数
+static void __exit hello_world_exit(void)
+{
+    printk(KERN_DEBUG "goodbye world!!!\n");
+}
+
+//将上述定义的init()和exit()函数定义为模块入口/出口函数
+module_init(hello_world_init);
+module_exit(hello_world_exit);
